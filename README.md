e02bc332b1a37d6e0bf93583891bff9b7a227b16
vs <santi.anjm@gmail.com>
2024-04-21 17:34:37 +0300

    Added README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Overview
+shortly about the  project
+
+# Main features
+matrix, list representations, used model for generating a graph,
+animation
+
+
+# How to set up
+like installing outer libraries
--- buildGraphList.py
+++ buildGraphList.py
@@ -1,23 +1,16 @@
 class GraphList:
     def __init__(self, vertices):
-        # Initialize the list of vertices
-        self.vertices = vertices
-        # Create the empty list of vertices
-        self.adjList = [[] for _ in range(vertices)]
+        self.vertices = vertices  # Initialize the list of vertices
+        self.adjList = [[] for _ in range(vertices)]  # Create the empty list of vertices
 
-    def addEdge(self, u, v):
-        # Simply adding the edge between u and v
+    def addEdge(self, u, v):  # Simply adding the edge between u and v
         self.adjList[u].append(v)
         # self.adjList[v].append(u)
 
     def getGraph(self):
         return self.adjList
 
-
     def showList(self):
         # Print the list of vertices
         for vertex, neighbors in enumerate(self.adjList):
             print(f"V_{vertex}: {neighbors}")
-
-
-
